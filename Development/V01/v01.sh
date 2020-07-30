7f016cf013290a8fd3e22825c5eae37558c02340
Warren Jitsing <warren.jitsing@gmail.com>
2020-07-30 12:41:42 +0200

    Add V01 app in development branch
    See: invictus-capital-engineering-assessment.pdf
    
    Why: Start designing base invictus application as per specification
    
    How: Docker Python Nameko
    
    Tags: V01 app

---
+++ Development/V01/v01.sh
@@ -0,0 +1,8 @@
+# Build a base Debian image
+docker image build -t base_debian -f base_debian .
+
+# Build the nameko base image
+docker image build -t nameko_base -f nameko_base .
+
+# Compile app base
+docker image build -t app_base -f app_base .
--- Development/test_messenger/temp_messenger.sh
+++ Development/test_messenger/temp_messenger.sh
@@ -1,5 +1,28 @@
-# Build a base Debian image
-docker image build -t nameko_temp_messenger .
+# Create nameko_base_messenger image from
+# nameko_base
+docker image build -t nameko_base_messenger .
+
+# Create the interview-net network
+docker network create --driver bridge interview-net
+
+# Check that network has been created
+docker network ls
+
+# Start rabbitmq instance on interview-net
+docker run -d --network interview-net --publish 5672:5672 \
+  --publish 15672:15672 --name rabbitmq_interview rabbitmq
+
+# Start redis server
+docker run -d --network interview-net  \
+  --publish 6379:6379 --name redis_interview redis
+
+# Check that both are running
+docker ps
+
+# Start nameko_base container
+docker container run -it \
+  --name driver_interview --network interview-net \
+  nameko_base_messenger /bin/bash
 
 # Change to app folder
 cd app
@@ -10,3 +33,6 @@ rm -rf invictus
 # Compile requirements
 pip-compile requirements/base.in
 pip-compile requirements/test.in
+
+# Run messenger service
+nameko run temp_messenger.service --config config.yaml &
