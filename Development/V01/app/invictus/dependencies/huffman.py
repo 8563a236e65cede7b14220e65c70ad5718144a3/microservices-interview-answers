53e3eb41023eb9a0de1dd7a4f3e5cec21df32785
Warren Jitsing <warren.jitsing@gmail.com>
2020-07-30 17:16:47 +0200

    Fix HuffmanCodec encode and decode for network use
    See: https://github.com/soxofaan/dahuffman/blob/master/dahuffman/huffmancodec.py
    
    Why: The trained codec needs to be transported over the network to the user.
         The codec contains a code_table that can be used to retrain the codec.
         The code_table must be serialized as some keys are binary.
    
    How: base64 pickle python3.7 dahuffman
    
    Tags: data transfer encoding decoding

--- Development/V01/app/invictus/dependencies/huffman.py
+++ Development/V01/app/invictus/dependencies/huffman.py
@@ -17,27 +17,34 @@ class HuffmanCoding:
     def __init__(self):
         self.str_list = list()
         self.result = dict()
-        self.encoded_dict = dict()
+        self.saved_codec = 0
 
     def encode(self, str_list):
         self.result = dict()
         self.str_list = str_list
         codec = dahuffman.HuffmanCodec.from_data("".join(self.str_list))
         for i in self.str_list:
+            print(i, codec.encode(i), codec.decode(codec.encode(i)))
             self.result.update({i: base64.b64encode(codec.encode(i)).decode("utf-8")})
         code_table = codec.get_code_table()
+        print(code_table)
         data = {
-            "code_table": code_table,
-            "type": type(codec),
-            "concat": codec._concat
+            "code_table": code_table
         }
         data = base64.b64encode(pickle.dumps(data)).decode("utf-8")
         self.result.update({"_codec": data})
+        #print(self.result)
         return self.result
 
-    def decode(self, encoded_dict, string):
-        self.encoded_dict = encoded_dict
-        return self.encoded_dict["_codec"].decode(string)
+    def decode(self, saved_codec, string):
+        self.saved_codec = saved_codec
+        self.saved_codec = base64.b64decode(self.saved_codec)
+        self.saved_codec = pickle.loads(self.saved_codec)
+        code_table = self.saved_codec["code_table"]
+        #print(frequencies)
+        codec = dahuffman.HuffmanCodec(code_table=code_table)
+        #print(base64.b64decode(string))
+        return "".join(codec.decode(base64.b64decode(string)))
 
 
 class HuffmanProvider(DependencyProvider):
